75ac8e1eeb2b4c7bfe0f367c38c881b57c19037a
2109106115_Rona Nurhijja Qonita <113363627+2109106115ronanurhijjaqonita@users.noreply.github.com>
2023-12-13 16:23:49 +0800

    Add files via upload

---
+++ profile/editprofile.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF333333),
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'Ubah profile',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 234, 175, 175),
+          ),
+        ),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: NetworkImage(
+                      'https://picsum.photos/250?image=9'),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Full Name',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(),
+            SizedBox(height: 16.0),
+            Text(
+              'Email Address',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Password',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Confirm Password',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Done'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ profile/profile.dart
@@ -0,0 +1,117 @@
+import 'package:flusix/screens/account/login.dart';
+import 'package:flusix/screens/profile/editprofile.dart';
+import 'package:flusix/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+              padding: const EdgeInsets.all(8.0),
+              children: <Widget>[
+          CircleAvatar(
+            radius: 60,
+            backgroundImage: NetworkImage(
+                'https://picsum.photos/250?image=9'),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Column(
+            children: [
+              Text(
+                'Rona',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'rona@gmail.com',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => EditProfile()));
+            },
+            child: Text('Edit profile'),
+          ),
+          SizedBox(height: 16,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.account_balance_wallet),
+              SizedBox(
+                width: 8.0,
+              ),
+              Text('My wallet')
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.language),
+            label: Text('Ubah bahasa'),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Icon(Icons.help_outline), Text('Pusat bantuan')],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text('Nilai aplikasi ini')
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 8.0,
+              ),
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SplashScreen()));
+                },
+                icon: Icon(Icons.logout),
+                label: Text('Logout'),
+              ),
+            ],
+          )
+              ],
+            ),
+        ));
+  }
+}
